d4df7f48a23ed76a461a702ead4dc2bb13b9cace
Jakub Zika <j@kub-zika.cz>
2011-11-19 17:53:51 +0100

    New macro die exits program in case of error

--- src/common.h
+++ src/common.h
@@ -9,4 +9,7 @@ void* alloc_(size_t size, const char* filename, unsigned int linenum);
 #define alloc(size) \
 		alloc_((size), __FILE__, __LINE__)
 
+#define die(status, errnum, ...) \
+		error_at_line((status), (errnum), __FILE__, __LINE__, __VA_ARGS__)
+
 #endif // NEGREP_COMMON_H_
