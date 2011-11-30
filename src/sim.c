07d4f3c86b331edf4dc4bc3c70b5fbe2dad1ec7e
Jakub Zika <j@kub-zika.cz>
2011-11-30 22:55:19 +0100

    Simulation is O(1) for precomputed edges

--- src/sim.c
+++ src/sim.c
@@ -71,7 +71,10 @@ static struct dfa_state* compute_dfa(struct sim_state* state,
 void sim_step(struct sim_state* state, uint_fast8_t chr) {
 	struct dfa_state* dfa_state = state->dfa_state;
 	struct dfa_state* next = dfa_state->edges[chr];
-	if (!next) next = compute_dfa(state, chr);
+	if (!next) {
+		next = compute_dfa(state, chr);
+		dfa_state->edges[chr] = next;
+	}
 	state->dfa_state = next;
 }
 
