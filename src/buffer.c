bdcde85bbb5634a22c1eeda382a98bcb102f469f
Jakub Zika <j@kub-zika.cz>
2011-12-06 17:14:28 +0100

    Fix memory leak in buffer.c

--- src/buffer.c
+++ src/buffer.c
@@ -13,7 +13,12 @@ struct buffer* buffer_fill(int fd, uintptr_t size) {
 		if (res->full == -1 && errno == EINTR) continue;
 		else break;
 	}
-	res->data = realloc(res->data, res->full);
+	if (res->full == 0) {
+		free(res->data);
+		res->data = 0;
+	} else {
+		res->data = realloc(res->data, res->full);
+	}
 	return res;
 }
 
