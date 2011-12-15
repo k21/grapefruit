35aaea5f6e58c5b5b1d8e0583565066ea8dca2b4
Jakub Zika <j@kub-zika.cz>
2011-12-15 22:49:37 +0100

    DFA cache tracks its memory usage

--- src/dfa.c
+++ src/dfa.c
@@ -13,11 +13,14 @@ struct dfa_cache_level {
 
 struct dfa_cache {
 	uintptr_t depth;
+	uintptr_t mem_usage;
 	struct dfa_cache_level root;
 };
 
-static struct dfa_cache_level* new_level(uintptr_t depth) {
+static struct dfa_cache_level* new_level(uintptr_t depth,
+		struct dfa_cache* cache) {
 	struct dfa_cache_level* res = alloc(sizeof(struct dfa_cache_level));
+	cache->mem_usage += sizeof(struct dfa_cache_level);
 	if (depth == 0) {
 		res->data.state = 0;
 	} else {
@@ -26,23 +29,26 @@ static struct dfa_cache_level* new_level(uintptr_t depth) {
 	return res;
 }
 
-static struct dfa_state* new_state(bool* active, uintptr_t active_length) {
+static struct dfa_state* new_state(bool* active, struct dfa_cache* cache) {
 	struct dfa_state* res = alloc(sizeof(struct dfa_state));
+	cache->mem_usage += sizeof(struct dfa_state);
 	uintptr_t i;
 	for (i = 0; i < 256; ++i) {
 		res->edges[i] = 0;
 	}
-	res->active = alloc(sizeof(bool)*active_length);
-	for (i = 0; i < active_length; ++i) {
+	res->active = alloc(sizeof(bool)*cache->depth);
+	cache->mem_usage += sizeof(bool)*cache->depth;
+	for (i = 0; i < cache->depth; ++i) {
 		res->active[i] = active[i];
 	}
-	res->accept = res->active[active_length-1];
+	res->accept = res->active[cache->depth-1];
 	return res;
 }
 
 struct dfa_cache* new_cache(uintptr_t depth) {
 	struct dfa_cache* res = alloc(sizeof(struct dfa_cache));
 	res->depth = depth;
+	res->mem_usage = 0;
 	if (depth == 0) {
 		res->root.data.state = 0;
 	} else {
@@ -52,11 +58,11 @@ struct dfa_cache* new_cache(uintptr_t depth) {
 }
 
 static struct dfa_state* get_impl(struct dfa_cache_level* level, bool* active,
-		uintptr_t depth, uintptr_t cache_depth) {
-	if (depth == cache_depth) {
+		uintptr_t depth, struct dfa_cache* cache) {
+	if (depth == cache->depth) {
 		struct dfa_state* dfa_state = level->data.state;
 		if (!dfa_state) {
-			dfa_state = new_state(active, cache_depth);
+			dfa_state = new_state(active, cache);
 			level->data.state = dfa_state;
 		}
 		return dfa_state;
@@ -64,14 +70,14 @@ static struct dfa_state* get_impl(struct dfa_cache_level* level, bool* active,
 	bool next_active = active[depth];
 	struct dfa_cache_level* next = level->data.child[next_active];
 	if (!next) {
-		next = new_level(cache_depth - depth);
+		next = new_level(cache->depth - depth, cache);
 		level->data.child[next_active] = next;
 	}
-	return get_impl(next, active, depth+1, cache_depth);
+	return get_impl(next, active, depth+1, cache);
 }
 
 struct dfa_state* cache_get(struct dfa_cache* cache, bool* active) {
-	return get_impl(&cache->root, active, 0, cache->depth);
+	return get_impl(&cache->root, active, 0, cache);
 }
 
 static void free_cache_level(struct dfa_cache_level* level, uintptr_t depth) {
